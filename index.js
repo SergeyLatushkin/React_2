4ecf1f1c3603cae215bc5eefafd3ce11c7bb8590
Siarhei Latushkin <Siarhei.Latushkin@pmi.org>
2020-01-27 11:11:08 +0300

    api client

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+class SwapiService {
+  _apiBase = 'https://swapi.co/api';
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
 
-console.log('hello world!');
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} received ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource('/people/');
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource('/planets/');
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource('/starships/');
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then(people => {
+  people.forEach(p => {
+    console.log(p.name);
+  });
+});
