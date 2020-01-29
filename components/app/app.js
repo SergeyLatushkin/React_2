b33ebf044f15bbaf63661404a910eed6ab0025a1
Siarhei Latushkin <Siarhei.Latushkin@pmi.org>
2020-01-29 16:39:33 +0300

    componentDidMount

--- components/app/app.js
+++ components/app/app.js
@@ -1,28 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
+import ErrorIndicator from '../error-indicator';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.css';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+export default class App extends Component {
+  state = {
+    showRandomPlanet: true,
+    hasError: false,
+    selectedPerson: null
+  };
 
-      <div className='row mb2'>
-        <div className='col-md-6'>
-          <ItemList />
-        </div>
-        <div className='col-md-6'>
-          <PersonDetails />
+  toggleRandomPlanet = () => {
+    this.setState(state => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      };
+    });
+  };
+
+  onPersonSelected = id => {
+    this.setState({
+      selectedPerson: id
+    });
+  };
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
+
+    return (
+      <div className='stardb-app'>
+        <Header />
+        {planet}
+
+        <div className='row mb2'>
+          <div className='col-md-6'>
+            <ItemList onItemSelected={this.onPersonSelected} />
+          </div>
+          <div className='col-md-6'>
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
--- components/random-planet/random-planet.js
+++ components/random-planet/random-planet.js
@@ -14,8 +14,7 @@ export default class RandomPlanet extends Component {
     loading: true
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
     this.interval = setInterval(this.updatePlanet, 2500);
     // clearInterval(this.interval);
