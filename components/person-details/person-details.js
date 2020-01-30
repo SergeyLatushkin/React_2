784c4af1d746873cf6b221b7472a7bdd116817f6
Siarhei Latushkin <Siarhei.Latushkin@pmi.org>
2020-01-30 15:39:16 +0300

    componentDidUpdate

--- components/person-details/person-details.js
+++ components/person-details/person-details.js
@@ -1,34 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
 
 import './person-details.css';
 
-const PersonDetails = () => {
+export default class PersonDetails extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    person: null,
+    selectedPerson: null,
+    loading: false
+  };
+
+  componentDidMount() {
+    this.updatePerson();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.personId !== prevProps.personId) {
+      this.setState({
+        loading: true
+      });
+      this.updatePerson();
+    }
+  }
+
+  updatePerson() {
+    const { personId } = this.props;
+
+    if (!personId) {
+      return;
+    }
+
+    this.swapiService.getPerson(personId).then(person => {
+      this.setState({
+        person,
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    const { person, loading } = this.state;
+
+    const hasNotContent =
+      !this.state.person && !loading ? (
+        <span>Select a person from a list</span>
+      ) : null;
+
+    const content =
+      !loading && this.state.person ? <PersonView person={person} /> : null;
+    const spinner = loading ? <Spinner /> : null;
+
+    return (
+      <div className='person-details card'>
+        {hasNotContent}
+        {spinner}
+        {content}
+      </div>
+    );
+  }
+}
+
+const PersonView = person => {
+  const {
+    person: { id, name, gender, birthYear, eyeColor }
+  } = person;
+
   return (
-    <div className='person-details card'>
+    <React.Fragment>
       <img
         className='person-image'
-        src='https://starwars-visualguide.com/assets/img/characters/3.jpg'
+        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
       />
-
       <div className='card-body'>
-        <h4>R2-D2</h4>
+        <h4>{name}</h4>
         <ul className='list-group list-group-flush'>
           <li className='list-group-item'>
             <span className='term'>Gender</span>
-            <span>male</span>
+            <span>{gender}</span>
           </li>
           <li className='list-group-item'>
             <span className='term'>Birth Year</span>
-            <span>43</span>
+            <span>{birthYear}</span>
           </li>
           <li className='list-group-item'>
             <span className='term'>Eye Color</span>
-            <span>red</span>
+            <span>{eyeColor}</span>
           </li>
         </ul>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
-
-export default PersonDetails;
--- components/random-planet/random-planet.js
+++ components/random-planet/random-planet.js
@@ -16,7 +16,7 @@ export default class RandomPlanet extends Component {
 
   componentDidMount() {
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 2500);
+    this.interval = setInterval(this.updatePlanet, 30000);
     // clearInterval(this.interval);
   }
 
