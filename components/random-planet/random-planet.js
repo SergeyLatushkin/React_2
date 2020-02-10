868f5d0a086874d67b5c5b0f83cdc627095bf21c
Sergey Latushkin (EPAM) <Siarhei_Latushkin@epam.com>
2020-02-10 11:19:18 +0300

    defaultProps

--- components/random-planet/random-planet.js
+++ components/random-planet/random-planet.js
@@ -7,6 +7,10 @@ import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
+  static defaultProps = {
+    updateInterval: 10000
+  };
+
   swapiService = new SwapiService();
 
   state = {
@@ -15,9 +19,10 @@ export default class RandomPlanet extends Component {
   };
 
   componentDidMount() {
+    const { updateInterval } = this.props;
+
     this.updatePlanet();
-    this.interval = setInterval(this.updatePlanet, 30000);
-    clearInterval(this.interval);
+    this.interval = setInterval(this.updatePlanet, updateInterval);
   }
 
   componentWillUnmount() {
